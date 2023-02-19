a1f01a5038b5ae52cb9dd3042436a002fa1281b7
Matan <0524615296b@gmail.com>
2023-02-19 14:56:26 +0200

    fix little thing 3

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,28 +1,28 @@
-import { useContext, useState, useEffect } from "react";
-import React from "react";
-import { auth } from "../firebase";
-const AuthContext = React.createContext();
+// import { useContext, useState, useEffect } from "react";
+// import React from "react";
+// import { auth } from "../firebase";
+// const AuthContext = React.createContext();
 
-export function useAuth() {
-  return useContext(AuthContext);
-}
-export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState();
-  useEffect(() => {
-    const unSubscribe = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user);
-      return unSubscribe;
-    });
-  }, []);
-  const value = { currentUser };
+// export function useAuth() {
+//   return useContext(AuthContext);
+// }
+// export function AuthProvider({ children }) {
+//   const [currentUser, setCurrentUser] = useState();
+//   useEffect(() => {
+//     const unSubscribe = auth.onAuthStateChanged((user) => {
+//       setCurrentUser(user);
+//       return unSubscribe;
+//     });
+//   }, []);
+//   const value = { currentUser };
 
-  const signUp = (password) => {
-    return auth.createUserWithPassword(password);
-  };
+//   const signUp = (password) => {
+//     return auth.createUserWithPassword(password);
+//   };
 
-  return (
-    <AuthContext.Provider value={(value, signUp)}>
-      {children}
-    </AuthContext.Provider>
-  );
-}
+//   return (
+//     <AuthContext.Provider value={(value, signUp)}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// }
